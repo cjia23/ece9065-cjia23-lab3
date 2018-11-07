61d771dfb0ec016530886baea6cad3a25de90d12
John Jia <cjia23@uwo.ca>
2018-11-07 03:04:56 +0000

    my commit message

---
+++ app/models/item.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ItemSchema = new Schema({
+    name: String
+    price: 
+});
+
+module.exports = mongoose.model('Item',ItemSchema);
+
+
--- server.js
+++ server.js
@@ -1,27 +1,100 @@
-'use strict';
+//Chunyang Jia's Lab 3, 251001556
 
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser"); 
-const cors = require("cors");
-let contacts = require("./data");
+// going to use express and body parser packages
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-const hostname = 'localhost';
-const port = 27017;
-const scheme = 'mongodb';
-const path = 'bears';
+//databaseg
+mongoose.connect('mongodb://localhost:27017/bears');
+var Bear = require('./app/models/bear');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/api/contacts', (request, response) => {
-    response.json(contacts);
+//Port set-up
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+
+router.use(function(req,res,next) {
+    console.log('Something is happening.');
+    next();
 });
 
-app.listen(port,hostname,() =>{
-    console.log(`Server is running at ${scheme}://${hostname}:${port}/${path}`);
+router.get('/',function(req, res) {
+  res.json({message: 'Woohoo, welcome to chunyang api!!!'});
 });
 
 
+router.route('/bears')
 
+        .post(function(req,res) {
+                
+                var bear = new Bear();
+                bear.name = req.body.name;
+        
+                bear.save(function(err) {
+                  if(err)
+                     res.send(err);
+                     
+                  res.json({message: 'Bear created!'});
+           });
+        })
+      
+        .get(function(req,res){
+            Bear.find(function(err,bears){
+                  if(err)
+                    res.send(err);
+                  
+                  res.json(bears);
+            });
+        });
+        
+router.route('/bears/:bear_id')
+        
+        .get(function(req, res){
+          
+            Bear.findById(req.params.bear_id,function(err,bear){
+                 if(err)
+                    res.send(err);
+                  
+                 res.json(bear);
+            });
+        })
+        
+        .put(function(req,res){
+          
+             Bear.findById(req.params.bear_id,function(err,bear){
+               if(err)
+                  res.send(err);
+                
+                bear.name = req.body.name;
+                
+                bear.save(function(err){
+                   if(err)
+                      res.send(err);
+                      
+                   res.json({message: 'Bear updated'});
+                });
+             });
+        })
+        
+        .delete(function(req,res){
+              Bear.remove({
+                _id: req.params.bear_id
+              }, function(err,bear){
+                  if(err)
+                      res.send(err);
+                  
+                  res.json({message: 'Successfully deleted'});
+              });
+        });
+      
 
+app.use('/api',router);
+
+app.listen(port);
+console.log('My server is running at: ' + port);
